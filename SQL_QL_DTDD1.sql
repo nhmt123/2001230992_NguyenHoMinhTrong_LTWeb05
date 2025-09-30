bf1d8ba2c12a49abd3e16e94dfcf2dbf48dc3a23
Nguyễn Hồ Minh Trọng <nhmt0406@gmail.com>
2025-09-30 23:28:14 +0700

    Add database file

---
+++ SQL_QL_DTDD1.sql
@@ -0,0 +1,79 @@
+﻿create database QL_DTDD1
+use QL_DTDD1
+
+create table Loai
+(
+	MaLoai int not null,
+	TenLoai nvarchar(50),
+	constraint pk_L primary key (MaLoai)
+)
+
+create table SanPham
+(
+	MaSP int not null,
+	TenSP nvarchar(50),
+	DuongDan nvarchar(50),
+	Gia float,
+	MoTa nvarchar(50),
+	MaLoai int,
+	constraint pk_SP primary key (MaSP),
+	constraint fk_SP_L foreign key (MaLoai) references Loai(MaLoai)
+)
+
+create table KhachHang
+(
+	MaKH int not null,
+	HoTen nvarchar(50),
+	DienThoai decimal,
+	GioiTinh nvarchar(5),
+	SoThich nvarchar(50),
+	Email nvarchar(50),
+	MatKhau nvarchar(50),
+	constraint pk_KH primary key (MaKH)
+)
+
+create table GioHang
+(
+	MaGH int not null,
+	MaKH int,
+	MaSP int,
+	SoLuong int,
+	Ngay Date,
+	constraint pk_GH primary key (MaGH),
+	constraint fk_GH_KH foreign key (MaKH) references KhachHang(MaKH),
+	constraint fk_GH_SP foreign key (MaSP) references SanPham(MaSP)
+)
+
+
+insert into Loai values
+(1, N'Nokia'),
+(2, N'SamSung'),
+(3, N'Motorola'),
+(4, N'LG'),
+(5, N'Oppo'),
+(6, N'Iphone'),
+(7, N'BPhone')
+
+insert into SanPham values
+(1, N'N701', N'Iphone.jpg', 2000000, N'Nâng cấp BN', 1),
+(2, N'N72', N'Iphone.jpg', 2100000, N'Nâng cấp BN, 2 màu Đen, Xám', 1),
+(3, N'N6030', N'Iphone.jpg', 3000000, N'Nâng cấp BN, Gấp', 1),
+(4, N'N6200', N'Iphone.jpg', 3200000, N'Nâng cấp BN, Màu Đen, Xám, Đỏ, Bạc', 1),
+(5, N'GalaxyA6', N'Iphone.jpg', 5200000, N'Nâng cấp BN, Màu Đen, Xám, Đỏ, Bạc', 2),
+(6, N'GalaxyA9', N'Iphone.jpg', 5500000, N'Nâng cấp BN, Màu Đen, Xám, Đỏ, Bạc', 2),
+(7, N'GalaxyJ5', N'Iphone.jpg', 6000000, N'Nâng cấp BN, Màu Đen, Xám, Đỏ, Bạc', 2),
+(16, N'MotoE5', N'Iphone.jpg', 2300000, N'Unlimited Extra', 3),
+(17, N'MotoG7', N'Iphone.jpg', 8000000, N'Unlimited Extra', 3),
+(24, N'Iphone4S', N'Iphone.jpg', 3000000, N'Không nâng cấp', 6),
+(25, N'Iphone5S', N'Iphone.jpg', 5000000, N'Không nâng cấp', 6),
+(26, N'Iphone6p', N'Iphone.jpg', 10000000, N'Không nâng cấp', 6),
+(27, N'Iphone7', N'Iphone.jpg', 15000000, N'Không nâng cấp', 6),
+(28, N'Iphone8p', N'Iphone.jpg', 20000000, N'Không nâng cấp', 6)
+
+drop table GioHang
+drop table SanPham
+drop table KhachHang
+drop table Loai
+
+
+
---
+++ SQL_QL_NhanSu.sql
@@ -0,0 +1,39 @@
+﻿create database QL_NhanSu
+use QL_NhanSu
+
+create table Department
+(
+	DeptId int not null,
+	Name nvarchar(50),
+	constraint pk_d primary key (DeptId)
+)
+
+create table Employee
+(
+	Id int not null,
+	Name nvarchar(50),
+	Gender nvarchar(5),
+	City nvarchar(50),
+	DeptId int,
+	constraint pk_e primary key (Id),
+	constraint fk_e_d foreign key (DeptId) references Department (DeptId)
+)
+
+insert into Department values
+(1, N'Khoa CNTT'),
+(2, N'Khoa Ngoại Ngữ'),
+(3, N'Khoa Tài Chính'),
+(4, N'Khoa Thực Phẩm'),
+(5, N'Phòng Đào Tạo')
+
+insert into Employee values
+(1, N'Nguyễn Hải Yến', N'Nữ', N'Đà Lạt', 1),
+(2, N'Trương Mạnh Hùng', N'Nam', N'TP.HCM', 1),
+(3, N'Đinh Duy Minh', N'Nam', N'Thái Bình', 2),
+(4, N'Ngô Thị Nguyệt', N'Nữ', N'Long An', 2),
+(5, N'Đào Minh Châu', N'Nữ', N'Bạc Liêu', 3),
+(14, N'Phan Thị Ngọc Mai', N'Nữ', N'Bến Tre', 3),
+(15, N'Trương Nguyễn Quỳnh Anh', N'Nữ', N'TP.HCM', 4),
+(16, N'Le Thanh Liêm', N'Nam', N'TP.HCM', 4)
+
+select * from Department
